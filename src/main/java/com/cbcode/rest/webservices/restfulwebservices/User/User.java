a335af7091ad3728abc22bc3109438ab9189571e
lucleber13 <lucleber13@hotmail.com>
2022-08-06 23:58:59 +0100

    Create class User

---
+++ src/main/java/com/cbcode/rest/webservices/restfulwebservices/User/User.java
@@ -0,0 +1,67 @@
+package com.cbcode.rest.webservices.restfulwebservices.User;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private Date birthDate;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(getId(), user.getId())
+                && Objects.equals(getName(), user.getName())
+                && Objects.equals(getBirthDate(), user.getBirthDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getBirthDate());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
